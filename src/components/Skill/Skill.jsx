d17dfb61c664b5a99b91c6c0d732144a608171f5
mohamedabdelaziz <moabdalaziz6116@gmail.com>
2023-03-10 05:47:45 +0200

    Some edits on the main button and removing the animation from the skills

--- src/components/Skill/Skill.jsx
+++ src/components/Skill/Skill.jsx
@@ -1,22 +1,16 @@
 import './Skill.scss';
-import { motion } from 'framer-motion';
 
 const Skill = ({ item }) => {
 
   return (
-    <motion.div
-      initial={{ opacity: 0, y: '48px' }}
-      whileInView={{ opacity: 1, y: '0' }}
-      transition={{ duration: 1 }}
-      className='skill'
-    >
+    <div className='skill'>
       <span className='skill-name me-2'>
         {item.skill}
       </span>
       <span className={`skill-icon ${item.colorClass}`}>
         {item.skillIcon}
       </span>
-    </motion.div>
+    </div>
   )
 }
 
